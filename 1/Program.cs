deeaf68c9617e34bf60c9d3577a216fc2c34fcac
Marina <marishka-2008-91@mail.ru>
2022-09-05 16:20:08 +0300

    1.3

--- 1/Program.cs
+++ 1/Program.cs
@@ -1,4 +1,4 @@
 ﻿Console.Write("Введите трехзначное число: ");
 int a = Convert.ToInt32(Console.ReadLine());
 int d = (a % 100)/10;
-Console.WriteLine(d);
+Console.WriteLine($"Значение в разряде десятков: {d}");
