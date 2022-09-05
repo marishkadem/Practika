b9e1a977f7b1de9b5cb27b29119c9ee09a091c70
Marina <marishka-2008-91@mail.ru>
2022-09-05 09:18:36 +0300

    1

---
+++ 1/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите трехзначное число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int d = a % 100-a % 10;
+if(d == 0){
+Console.WriteLine(d);
+}
+else{
+int c = d / 10;
+Console.WriteLine(c);
+}
