7e25e5198b6ed978d5557810fe32304e34aed287
Marina <marishka-2008-91@mail.ru>
2022-09-05 15:46:59 +0300

    2

---
+++ 2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число: ");
+long a = Convert.ToInt64(Console.ReadLine());
+long c = a / 100;
+if(c == 0){         
+    Console.WriteLine("В разряде сотен - ноль");
+}
+else if (c > 10){
+    c = c % 10;
+    Console.WriteLine($"Цифра в разряде сотен равна {c}");  
+}
+else {
+    Console.WriteLine($"Цифра в разряде сотен равна {c}");
+}
