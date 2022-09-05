fe0046d63c007b8be4b0c859afd5dfc6b2df867c
Marina <marishka-2008-91@mail.ru>
2022-09-05 16:20:51 +0300

    3

---
+++ 3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите день недели: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if(a<6) {
+    Console.WriteLine("Рабочий день");
+}
+else if(a>7) {
+    Console.WriteLine("В неделе 7 дней");
+}
+else {
+   Console.WriteLine("Выходной"); 
+}
